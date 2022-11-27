0848d4d85d6c7f39fc77858faaeae5bdba0b4766
Karol Jajko <karoljajko@gmail.com>
2022-11-27 13:42:20 +0100

    Added media size

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,129 @@
-const allAngWord = document.querySelectorAll('.table__word-ang')
-const allArrowIcon = document.querySelectorAll('.icons')
-const arrowIcon = document.querySelectorAll('.fa-solid')
-const textForm = document.querySelectorAll('.table__info__form__text')
+// POBRANE ELEMENTY
+const elementArrowIcon = document.querySelectorAll('.icons__arrow')
+const elementPlusIcon = document.querySelectorAll('.icon__plus')
+const elementMinusIcon = document.querySelectorAll('.icon__minus')
+const arrowIcons = document.querySelectorAll('.icons__arrow p')
 
+const titleForms = document.querySelectorAll('.table__info__form__title')
+const textForms = document.querySelectorAll('.table__info__form__text')
+
+const allAngWord = document.querySelectorAll('.table__word-ang')
+
+const angSeeNoOne = document.querySelectorAll('.ang__see-no--one')
+const angSeeNoTwo = document.querySelectorAll('.ang__see-no--two')
+const angSeeNoThree = document.querySelectorAll('.ang__see-no--three')
+
+const angSeeOne = document.querySelectorAll('.ang__see--one')
+const angSeeTwo = document.querySelectorAll('.ang__see--two')
+const angSeeThree = document.querySelectorAll('.ang__see--three')
+
+// FUNKCJE DLA ROZWIJANEGO TEKSTU
+const seeText0 = () => {
+	textForms[0].classList.toggle('text-see')
+	titleForms[0].classList.toggle('title-margin')
+	titleForms[0].classList.toggle('title-margin-second')
+	arrowIcons[0].classList.toggle('icons-rotate')
+}
+const seeText1 = () => {
+	textForms[1].classList.toggle('text-see')
+	titleForms[1].classList.toggle('title-margin')
+	titleForms[1].classList.toggle('title-margin-second')
+	arrowIcons[1].classList.toggle('icons-rotate')
+}
+const seeText2 = () => {
+	textForms[2].classList.toggle('text-see')
+	titleForms[2].classList.toggle('title-margin')
+	titleForms[2].classList.toggle('title-margin-second')
+	arrowIcons[2].classList.toggle('icons-rotate')
+}
+
+elementArrowIcon[0].addEventListener('click', seeText0)
+elementArrowIcon[1].addEventListener('click', seeText1)
+elementArrowIcon[2].addEventListener('click', seeText2)
+
+// FUNKCJA DO POJEDYNCZYCH SŁÓW 
 for (let word of allAngWord) {
-	const seeAngWord = e => {
-		e.target.classList.toggle('ang-see')
+	const seeAngWord = (e) => {
+		if(e.target.classList.contains('ang__see--one') || e.target.classList.contains('ang__see-no--one')) {
+			e.target.classList.toggle('ang__see--one')
+			e.target.classList.toggle('ang__see-no--one')
+		} else if (e.target.classList.contains('ang__see--two') || e.target.classList.contains('ang__see-no--two')) {
+			e.target.classList.toggle('ang__see--two')
+			e.target.classList.toggle('ang__see-no--two')
+		} else if (e.target.classList.contains('ang__see--three') || e.target.classList.contains('ang__see-no--three')) {
+			e.target.classList.toggle('ang__see--three')
+			e.target.classList.toggle('ang__see-no--three')
+		}
 	}
+
 	word.addEventListener('click', seeAngWord)
 }
 
-const seeT0 = () => {
-	textForm[0].classList.toggle('text-see')
-    arrowIcon[0].classList.toggle('icons-rotate')
-}
-const seeT1 = () => {
-	textForm[1].classList.toggle('text-see')
-    arrowIcon[1].classList.toggle('icons-rotate')
-}
-const seeT2 = () => {
-	textForm[2].classList.toggle('text-see')
-    arrowIcon[2].classList.toggle('icons-rotate')
+// FUNKCJA DO ZBIOROWYCH SŁÓW 
+const seeWordOne = () => {
+	angSeeNoOne.forEach(wordOne => {
+		if (wordOne.classList.contains('ang__see-no--one')) {
+			wordOne.classList.toggle('ang__see--one')
+			wordOne.classList.toggle('ang__see-no--one')
+		}
+	})
 }
 
-allArrowIcon[0].addEventListener('click', seeT0)
-allArrowIcon[1].addEventListener('click', seeT1)
-allArrowIcon[2].addEventListener('click', seeT2)
+const noSeeWordOne = () => {
+	const angSeeOne = document.querySelectorAll('.ang__see--one')
+	angSeeOne.forEach(wordOneNo => {
+		if (wordOneNo.classList.contains('ang__see--one')) {
+			wordOneNo.classList.toggle('ang__see--one')
+			wordOneNo.classList.toggle('ang__see-no--one')
+		}
+	})
+}
+
+const seeWordTwo = () => {
+	angSeeNoTwo.forEach(wordTwo => {
+		if (wordTwo.classList.contains('ang__see-no--two')) {
+			wordTwo.classList.toggle('ang__see--two')
+			wordTwo.classList.toggle('ang__see-no--two')
+		}
+	})
+}
+
+const noSeeWordTwo = () => {
+	const angSeeTwo = document.querySelectorAll('.ang__see--two')
+	angSeeTwo.forEach(wordTwoNo => {
+		if (wordTwoNo.classList.contains('ang__see--two')) {
+			wordTwoNo.classList.toggle('ang__see--two')
+			wordTwoNo.classList.toggle('ang__see-no--two')
+		}
+	})
+}
+
+const seeWordThree = () => {
+	angSeeNoThree.forEach(wordThree => {
+		if (wordThree.classList.contains('ang__see-no--three')) {
+			wordThree.classList.toggle('ang__see--three')
+			wordThree.classList.toggle('ang__see-no--three')
+		}
+	})
+}
+
+const noSeeWordThree = () => {
+	const angSeeThree = document.querySelectorAll('.ang__see--three')
+	angSeeThree.forEach(wordThreeNo => {
+		if (wordThreeNo.classList.contains('ang__see--three')) {
+			wordThreeNo.classList.toggle('ang__see--three')
+			wordThreeNo.classList.toggle('ang__see-no--three')
+		}
+	})
+}
+
+elementPlusIcon[0].addEventListener('click', seeWordOne)
+elementMinusIcon[0].addEventListener('click', noSeeWordOne)
+
+elementPlusIcon[1].addEventListener('click', seeWordTwo)
+elementMinusIcon[1].addEventListener('click', noSeeWordTwo)
+
+elementPlusIcon[2].addEventListener('click', seeWordThree)
+elementMinusIcon[2].addEventListener('click', noSeeWordThree)
+
 
