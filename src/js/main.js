a609d8dfd5a80e5d1ab9488a609ae3f54adf5857
Karol Jajko <karoljajko@gmail.com>
2024-07-23 18:04:42 +0200

    added cookie & google tag

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,32 @@
+const cookieBox = document.querySelector('.cookie')
+const cookieBtn = document.querySelector('.cookie-btn')
+const disclaimerBtn = document.querySelector('.disclaimer-btn')
+const disclaimerText = document.querySelector('.disclaimer-text')
+
+const showCookie = () => {
+	const cookieEaten = localStorage.getItem('cookie')
+	if (cookieEaten) {
+		cookieBox.classList.add('hide-cookies')
+	}
+}
+
+const handleCookieBox = () => {
+	localStorage.setItem('cookie', 'true')
+	cookieBox.classList.add('hide-cookies')
+}
+
+const handleCookieBoxDisclaimer = () => {
+	disclaimerText.classList.toggle('hide-cookies')
+}
+
 const footerYear = document.querySelector('.footer__year')
 const handleCurrentYear = () => {
 	const year = new Date().getFullYear()
 	footerYear.innerText = year
 }
+
+cookieBtn.addEventListener('click', handleCookieBox)
+disclaimerBtn.addEventListener('click', handleCookieBoxDisclaimer)
+
+showCookie()
 handleCurrentYear()
